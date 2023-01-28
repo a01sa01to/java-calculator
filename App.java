0345e97d21ae1ebaa4379e14ef7bb915efce7b5f
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-01-28 11:56:21 +0900

    Add buttons

--- App.java
+++ App.java
@@ -1,26 +1,31 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
-import javafx.scene.text.Text;
 
 public class App extends Application {
   @Override
   public void start(Stage primaryStage) {
+    // Initialize App
     primaryStage.setTitle("Calculator");
     primaryStage.setResizable(false);
-
     BorderPane root = new BorderPane();
-    root.setPrefSize(200, 300);
+    root.setPrefSize(250, 350);
+
+    // Button Display
+    GridPane gridPane = new GridPane();
+    Button[] buttons = new Button[25];
+    for (int i = 0; i < 25; i++) {
+      buttons[i] = new Button();
+      buttons[i].setPrefSize(50, 50);
+      buttons[i].setText(Integer.toString(i));
+      gridPane.add(buttons[i], i / 5, i % 5);
+    }
+
+    root.setCenter(gridPane);
+
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
